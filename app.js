d5871437b975f8a67268a929a95c6f5bf3d8984f
beastsunny <beastsunny06@gmail.com>
2021-09-12 10:25:33 +0530

    Doc: Array added

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ var cashInput = document.querySelector(".cash-input")
 var cashGiven = document.querySelector("#cash-given")
 var divOutput = document.querySelector("#output")
 var returnTable = document.querySelector(".return-table")
+
+
+const arrayOfNotes = [2000, 500, 100, 20, 10, 5, 1]
+console.log(arrayOfNotes.length)
